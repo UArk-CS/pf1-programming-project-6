8fc7ba790dd95c0ba2fe70ce7304deb2cbe4022f
Benjamin Worthington <benworthin@icloud.com>
2020-11-14 02:05:24 -0600

    Added functions to Pokemon class definition

--- Pokemon.h
+++ Pokemon.h
@@ -10,6 +10,7 @@ private:
     std::string name;
     std::string gender;
     std::string type;
+    std::string color;
     float weight;
     float height;
 
@@ -17,6 +18,9 @@ public:
     Pokemon();
     ~Pokemon();
 
+    void heightAdjustment(const float height);
+    void liposuction(const float weight);
+    void sprayPaint(const std::string color);
     void print() const;
 
 };
